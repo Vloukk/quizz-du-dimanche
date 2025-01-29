8911f233cd047cb70d49d2a5181e301ddff9a40c
Vloukk <v.valentin.evangelista@gmail.com>
2025-01-29 17:13:01 +0100

    creation d'un sessionID manuel + creation de lien partagabe + test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // 📌 Ajoute cette ligne pour gérer les routes
+  }
+});
